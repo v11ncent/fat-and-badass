84ee5bbc346aa0b654a04756c3c45f4efb2de526
Vince Corradetti <vince.corradetti@auxosolutions.io>
2025-04-13 23:28:26 -0400

    Added favicon and og image

---
+++ web/assets/scripts/index.js
@@ -0,0 +1,12 @@
+const accentColor = getComputedStyle(document.documentElement).getPropertyValue(
+  "--clr-accent"
+);
+const titleWords = document.querySelectorAll("h1 span");
+
+if (titleWords) {
+  
+}
+
+const getRandomTime = (max) => {
+  return Math.floor(Math.random() * max);
+};
